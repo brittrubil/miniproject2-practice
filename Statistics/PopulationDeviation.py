d3126961e11b152b8ef4a02fa4b57bdc2a689956
Brittani Rubil <bmb23@njit.edu>
2019-11-15 16:35:56 -0500

    Fix: Fixed Population Standard Deviation and added exception handling

--- Statistics/PopulationDeviation.py
+++ Statistics/PopulationDeviation.py
@@ -1,13 +1,17 @@
 from statistics import pstdev
 
 
-def population_deviation(a, b , c, d, e, f):
-    a = float(a)
-    b = float(b)
-    c = float(c)
-    d = float(d)
-    e = float(e)
-    f = float(f)
-
-    num_list = [a, b, c, d, e, f]
-    return round(pstdev(num_list), 2)
+def population_deviation(a, b, c, d, e, f):
+    try:
+        a = float(a)
+        b = float(b)
+        c = float(c)
+        d = float(d)
+        e = float(e)
+        f = float(f)
+        num_list = [a, b, c, d, e, f]
+        return round(pstdev(num_list), 2)
+    except ZeroDivisionError:
+        print("Error: Number Not Valid")
+    except ValueError:
+        print("Error: Only Numeric Values")
