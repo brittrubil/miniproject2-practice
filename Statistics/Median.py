cd36a2edf780e658a9ced40c2eb5fa8b1dacc29d
Brittani Rubil <bmb23@njit.edu>
2019-11-15 13:58:58 -0500

    Task: Added Exception Handling

--- Statistics/Median.py
+++ Statistics/Median.py
@@ -1,17 +1,22 @@
-def median(a, b , c, d):
-    a = int(a)
-    b = int(b)
-    c = int(c)
-    d = int(d)
+def median(a, b, c, d):
+    try:
+        a = int(a)
+        b = int(b)
+        c = int(c)
+        d = int(d)
 
-    num_list = [a, b, c, d]
-    # Sort the list
-    num_list.sort()
-    # Finding the position of the median
-    if len(num_list) % 2 == 0:
-        first_median = num_list[len(num_list) // 2]
-        second_median = num_list[len(num_list) // 2 - 1]
-        median = (first_median + second_median) / 2
-    else:
-        median = num_list[len(num_list) // 2]
-    return median
+        num_list = [a, b, c, d]
+        # Sort the list
+        num_list.sort()
+        # Finding the position of the median
+        if len(num_list) % 2 == 0:
+            first_median = num_list[len(num_list) // 2]
+            second_median = num_list[len(num_list) // 2 - 1]
+            result_median = (first_median + second_median) / 2
+        else:
+            result_median = num_list[len(num_list) // 2]
+        return result_median
+    except ZeroDivisionError:
+        print("Error: Number Not Valid")
+    except ValueError:
+        print("Error: Only Numeric Values")
