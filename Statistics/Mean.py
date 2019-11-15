5863f140f9744d0aa381eb3858d16f444c856798
Brittani Rubil <bmb23@njit.edu>
2019-11-15 13:50:58 -0500

    Fix: Fixed Mean and added Exception Handling

--- Statistics/Mean.py
+++ Statistics/Mean.py
@@ -1,9 +1,11 @@
-from Calculator.Addition import addition
-from Calculator.Division import division
-
-def mean(a, b):
-    a = int(a)
-    b = int(b)
-    c = int(a + b)
-    return c / 2
-
+def mean(a, b, c):
+    try:
+        a = int(a)
+        b = int(b)
+        c = int(c)
+        d = (a + b) / c
+        return d
+    except ZeroDivisionError:
+        print("Error: Number Not Valid")
+    except ValueError:
+        print("Error: Only Numeric Values")
