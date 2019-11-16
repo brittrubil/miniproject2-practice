bed31e8160a6c33e403e00992d60a0b792a2beca
Brittani Rubil <bmb23@njit.edu>
2019-11-15 19:46:47 -0500

    Task: Added exception handling

--- Statistics/Deviation.py
+++ Statistics/Deviation.py
@@ -2,11 +2,15 @@ from statistics import stdev
 
 
 def deviation(a, b , c, d, e):
-    a = int(a)
-    b = int(b)
-    c = int(c)
-    d = int(d)
-    e = int(e)
-
-    num_list = [a, b, c, d, e]
-    return round(stdev(num_list), 2)
+    try:
+        a = int(a)
+        b = int(b)
+        c = int(c)
+        d = int(d)
+        e = int(e)
+        num_list = [a, b, c, d, e]
+        return round(stdev(num_list), 2)
+    except ZeroDivisionError:
+        print("Error: Number Not Valid")
+    except ValueError:
+        print("Error: Only Numeric Values")
