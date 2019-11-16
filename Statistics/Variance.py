ff25645106856f22d6452861700e3903fa3f3ff4
Brittani Rubil <bmb23@njit.edu>
2019-11-15 19:39:48 -0500

    Task: Added exception handling

--- Statistics/Variance.py
+++ Statistics/Variance.py
@@ -2,14 +2,18 @@ from statistics import pvariance
 
 
 def variance(a, b, c, d, e, f, g, h):
-    a = float(a)
-    b = float(b)
-    c = float(c)
-    d = float(d)
-    e = float(e)
-    f = float(f)
-    g = float(g)
-    h = float(h)
-
-    num_list = [a, b, c, d, e, f, g, h]
-    return pvariance(num_list)
+    try:
+        a = float(a)
+        b = float(b)
+        c = float(c)
+        d = float(d)
+        e = float(e)
+        f = float(f)
+        g = float(g)
+        h = float(h)
+        num_list = [a, b, c, d, e, f, g, h]
+        return pvariance(num_list)
+    except ZeroDivisionError:
+        print("Error: Number Not Valid")
+    except ValueError:
+        print("Error: Only Numeric Values")
