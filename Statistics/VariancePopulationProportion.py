55e130c5f2018b4eef623b05d6a3a34d5454f7be
Brittani Rubil <bmb23@njit.edu>
2019-11-15 19:20:00 -0500

    Task: Added Variance of Population Proportion

---
+++ Statistics/VariancePopulationProportion.py
@@ -0,0 +1,10 @@
+def populationproportion(a, b):
+    try:
+        a = int(a)
+        b = int(b)
+        c = ((a ** 2) / b) ** 2
+        return c
+    except ZeroDivisionError:
+        print("Error: Number Not Valid")
+    except ValueError:
+        print("Error: Only Numeric Values")
